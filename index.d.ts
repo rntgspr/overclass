eeb35d99a38aaf576942491b91fd921888049d5d
Renato Gaspar <rntgspr@gmail.com>
2023-05-18 13:45:42 -0700

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+export declare function oc(
+
+  styles: Record<string, string>
+
+): (literal: string[], ...args: string[]) => string;
+
+export declare function cn(literal: string[], ...args: string[]): string;
+
+export declare function jn(...args: string[]): string;
